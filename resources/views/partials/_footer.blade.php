284db630b283c92663b61a837173dcf93a30b3ca
Orestis <orestiszag@gmail.com>
2016-07-11 18:16:06 +0300

    base file structure for front-end created and bootstrap added.

---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,6 @@
+<hr>
+<p>&copy All rights reserved. This is a thesis project for <a href="http://www.teicm.gr/">Technological Educationl
+   Institute of Central Macedonia</a> <a href="http://informatics.teicm.gr/">IT Department</a>.
+   Created by <a href="https://gr.linkedin.com/in/orestis-zagkoumidis-7a4071b6">Orestis Zagkoumidis</a> and
+   <a href="https://gr.linkedin.com/in/konstantina-avgeri-189b82121">Konstantina Avgeri</a>.
+</p>
