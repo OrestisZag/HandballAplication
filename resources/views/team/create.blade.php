1cb52273f0dc07713df13966d8792cf9c12b402b
Orestis <orestiszag@gmail.com>
2016-09-21 16:17:02 +0300

    parsley added on teams create

--- resources/views/team/create.blade.php
+++ resources/views/team/create.blade.php
@@ -2,34 +2,46 @@
 
 @section('title', '| Add New Team')
 
+@section('style')
+    {!! Html::style('css/parsley.css') !!}
+@endsection
+
 @section('content')
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-4 col-md-offset-4">
             <h1>Add New Team</h1>
-            {!! Form::open(['route' => 'team.store']) !!}
+            {!! Form::open(['route' => 'team.store', 'data-parsley-validate' => '', 'class' => 'form']) !!}
                 {{ Form::label('name', 'Name:') }}
-                {{ Form::text('name', null, ['class' => 'vale bs class']) }}
+                {{ Form::text('name', null, ['class' => 'form-control input-sm', 'required' => '',
+                   'minlength' => '2', 'maxlength' => '50']) }}
 
                 {{ Form::label('level', 'Level:') }}
-                {{ Form::text('level', null, ['class' => 'vale bs class']) }}
+                {{ Form::text('level', null, ['class' => 'form-control input-sm', 'required' => '']) }}
 
                 {{ Form::label('place', 'Place:') }}
-                {{ Form::text('place', null, ['class' => 'vale bs class']) }}
+                {{ Form::text('place', null, ['class' => 'form-control input-sm', 'required' => '',
+                   'minlength' => '2', 'maxlength' => '100']) }}
 
                 {{ Form::label('telephone', 'Telephone:') }}
-                {{ Form::tel('telephone', null, ['class' => 'vale bs class']) }}
+                {{ Form::tel('telephone', null, ['class' => 'form-control input-sm', 'minlength' => '10',
+                   'maxlength' => '15']) }}
 
                 {{ Form::label('fax', 'Fax:') }}
-                {{ Form::tel('fax', null, ['class' => 'vale bs class']) }}
+                {{ Form::tel('fax', null, ['class' => 'form-control input-sm', 'minlength' => '10',
+                   'maxlength' => '15']) }}
 
                 {{ Form::label('email', 'Email:') }}
-                {{ Form::email('email', null, ['class' => 'vale bs class']) }}
+                {{ Form::email('email', null, ['class' => 'form-control input-sm', 'email' => '']) }}
 
                 {{ Form::label('website', 'Website') }}
-                {{ Form::text('website', null, ['class' => 'vale bs class']) }}
+                {{ Form::text('website', null, ['class' => 'form-control input-sm', 'url' => '']) }}
 
-                {{ Form::submit('Add Team', ['class' => 'vale bs class']) }}
+                {{ Form::submit('Add Team', ['class' => 'btn btn-success btn-block space-top']) }}
             {!! Form::close() !!}
         </div>
     </div>
+@endsection
+
+@section('script')
+    {!! Html::script('js/parsley.min.js') !!}
 @endsection
