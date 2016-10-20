d758c2c64ea925b24af50a4e845bf641861afcfe
fireandrew <fire-andrew@hotmail.com>
2016-10-20 20:08:32 +0300

    implement pdf export

--- resources/views/partials/_javascript.blade.php
+++ resources/views/partials/_javascript.blade.php
@@ -5,3 +5,11 @@
 0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
 <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.debug.js"></script>
+
+{!! Html::script('js/zlib.js') !!}
+{!! Html::script('js/png.js') !!}
+{!! Html::script('js/addImage.js') !!}
+{!! Html::script('js/png_support.js') !!}
+{!! Html::script('js/html2canvas.js') !!}
--- resources/views/pdf/campAthleteEval.blade.php
+++ resources/views/pdf/campAthleteEval.blade.php
@@ -5,23 +5,48 @@
 @section('content')
     <div class="row">
         <div class="col-md-6 col-md-offset-3 space-top">
-            <ul class="list-group">
-                <li class="list-group-item text-center"><strong>Name: </strong>{{ $name }}</li>
-                <li class="list-group-item text-center"><strong>Train Date: </strong>{{ $train->date }}</li>
-                <li class="list-group-item text-center"><strong>Position: </strong>{{ $train->position->fullName }}</li>
-                <li class="list-group-item text-center"><strong>Attack Evaluation: </strong>{{ $train->attackEval }}</li>
-                <li class="list-group-item text-center"><strong>Defence Evaluation: </strong>{{ $train->defenceEval }}</li>
-                <li class="list-group-item text-center"><strong>Attack + Defence Evaluation: </strong>{{ $train->atDefEval }}</li>
-                <li class="list-group-item text-center"><strong>Comments: </strong><div class="panel-body">{{ $train->comments }}</div>
-                    </li>
-            </ul>
+            <button id="cmd" class="btn btn-success btn-block">Download PDF</button>
         </div>
     </div>
-    <div class="row">
-        <div class="col-md-6 col-md-offset-3">
-            <div id="chart-div">
-                {!! $lava !!}
+    <div id="pdf-content">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3 space-top">
+                <ul class="list-group">
+                    <li class="list-group-item text-center"><strong>Name: </strong>{{ $name }}</li>
+                    <li class="list-group-item text-center"><strong>Train Date: </strong>{{ $train->date }}</li>
+                    <li class="list-group-item text-center"><strong>Position: </strong>{{ $train->position->fullName }}
+                    </li>
+                    <li class="list-group-item text-center"><strong>Attack Evaluation: </strong>{{ $train->attackEval }}
+                    </li>
+                    <li class="list-group-item text-center"><strong>Defence
+                            Evaluation: </strong>{{ $train->defenceEval }}</li>
+                    <li class="list-group-item text-center"><strong>Attack + Defence
+                            Evaluation: </strong>{{ $train->atDefEval }}</li>
+                    <li class="list-group-item text-center"><strong>Comments: </strong>
+                        <div class="panel-body">{{ $train->comments }}</div>
+                    </li>
+                </ul>
             </div>
         </div>
+        {{--<div class="row">--}}
+            {{--<div class="col-md-6 col-md-offset-3">--}}
+                {{--<div id="chart-div">--}}
+                    {{--{!! $lava !!}--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
     </div>
+@endsection
+
+@section('script')
+    <script>
+        $("#pdf-content").css("background-color", "white");
+        var doc = new jsPDF('l', 'mm', [297, 210]);
+
+        $('#cmd').click(function () {
+            doc.addHTML($('#pdf-content'), function() {
+                doc.save('{{$name}}' + ' camp evaluation {{$train->date}}.pdf');
+            });
+        });
+    </script>
 @endsection
