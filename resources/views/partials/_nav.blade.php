57ba01af7ba1af61883d59551298b1542d3b953f
Orestis <orestiszag@gmail.com>
2016-10-21 20:31:22 +0300

    login button is ready. It will be uncommented when auth is set

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -33,17 +33,22 @@
                 {{--</li>--}}
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                {{--<li><a href="#">Link</a></li>--}}
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="{{ route('athlete.index') }}">Athletes</a></li>
-                        <li><a href="{{ route('team.index') }}">Teams</a></li>
-                        <li><a href="{{ route('camp.index') }}">Camp</a></li>
-                        {{--<li role="separator" class="divider"></li>--}}
-                        {{--<li><a href="#">Separated link</a></li>--}}
-                    </ul>
-                </li>
+                {{--@if(Auth::check())--}}
+                    {{--<li class="dropdown">--}}
+                        {{--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"--}}
+                           {{--aria-expanded="false">Hello {{ Auth::user()->name }}--}}
+                            {{--<span class="caret"></span></a>--}}
+                        {{--<ul class="dropdown-menu">--}}
+                            {{--<li><a href="#">smth</a></li>--}}
+                            {{--<li><a href="#">smth</a></li>--}}
+                            {{--<li><a href="#">smth</a></li>--}}
+                            {{--<li role="separator" class="divider"></li>--}}
+                            {{--<li><a href="{{ route('logout') }}">Logout</a></li>--}}
+                        {{--</ul>--}}
+                    {{--</li>--}}
+                {{--@else--}}
+                    {{--<a href="{{ route('login') }}" class="btn btn-default" style="margin-top: 7px;">Login</a>--}}
+                {{--@endif--}}
             </ul>
         </div>
     </div>
