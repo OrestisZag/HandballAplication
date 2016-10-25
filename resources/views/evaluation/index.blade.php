ed6fe5cdf57e51783d991b6b4bb8b905c316764c
Orestis <orestiszag@gmail.com>
2016-10-25 22:53:45 +0300

    minor refactor

--- resources/views/evaluation/index.blade.php
+++ resources/views/evaluation/index.blade.php
@@ -4,28 +4,28 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-7 col-md-offset-3">
+        <div class="col-md-6 col-md-offset-3">
             <a href="{{ route('evaluation.info') }}" class="btn btn-primary center-block">Create New Athlete's Evaluation</a>
             <table class="table">
                 <thead>
                 <tr>
-                    <th>Athlete</th>
-                    <th>Match</th>
-                    <th>Date</th>
+                    <th class="text-center">Athlete</th>
+                    <th class="text-center">Match</th>
+                    <th class="text-center">Date</th>
                     <th></th>
                 </tr>
                 </thead>
                 <tbody>
                 @foreach($entities as $entity)
                             <tr>
-                            <td>{{ $entity['athleteLastName'] }} {{ $entity['athleteFirstName'] }}</td>
-                            <td>{{ $entity['home'] }} - {{ $entity['home'] }}</td>
-                            <td>{{ $entity['date'] }} </td>
+                                <td class="text-center">{{ $entity['athleteLastName'] }} {{ $entity['athleteFirstName'] }}</td>
+                                <td class="text-center">{{ $entity['home'] }} - {{ $entity['home'] }}</td>
+                                <td class="text-center">{{ $entity['date'] }} </td>
 
-                            <td>
-                                <a href="{{ route('evaluation.show', $entity['evalId']) }}" class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-info-sign"></span></a>
-                                <a href="{{ route('evaluation.edit', $entity['evalId']) }}" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-edit"></span></a>
-                            </td>
+                                <td>
+                                    <a href="{{ route('evaluation.show', $entity['evalId']) }}" class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-info-sign"></span></a>
+                                    <a href="{{ route('evaluation.edit', $entity['evalId']) }}" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-edit"></span></a>
+                                </td>
                             </tr>
                 @endforeach
                 </tbody>
