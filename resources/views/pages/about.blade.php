56422e99c81b4a220891ce7b3bc4441978ab8f6d
konavge <konavge93@gmail.com>
2016-10-26 21:43:42 +0300

    about refactor, add logo on title

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,16 +5,18 @@
 @section('content')
     <div class="row">
         <div class="col-md-6 col-md-offset-3 text-center">
+            <div class="jumbotron">
             <h1 class="text-center">About Us</h1>
             <h2><u>Students</u></h2>
             <h3>Student 1:</h3>
-            <label><u>Last Name: </u></label><strong> Zagkoumidis</strong><br>
-            <label><u>First Name: </u></label><strong> Orestis</strong><br>
-            <label><u>AEM: </u></label><strong> 3322</strong><br>
+            <label>Last Name: </label><strong> Zagkoumidis</strong><br>
+            <label>First Name: </label><strong> Orestis</strong><br>
+            <label>AEM: </label><strong> 3322</strong><br>
             <h3>Student 2:</h3>
-            <label><u>Last Name: </u></label><strong> Avgeri</strong><br>
-            <label><u>First Name: </u></label><strong> Konstantina</strong><br>
-            <label><u>AEM: </u></label><strong> 3414</strong><br><br>
+            <label>Last Name: </label><strong> Avgeri</strong><br>
+            <label>First Name: </label><strong> Konstantina</strong><br>
+            <label>AEM: </label><strong> 3414</strong><br><br>
+            </div>
             <p class="text-center">Current website is our Thesis project for Technological Educational Institute
                 of Central Macedonia.</p>
             <p class="text-center">We would like to thank our supervisor Dr. Tsimpiris Alkividis,
--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -3,6 +3,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 <title>Handball App @yield('title')</title>
+<link rel="icon" href="{!! asset('img/HandLogo.png') !!}"/>
+
 {{--title is not permanently--}}
 
 <!-- Bootstrap -->
